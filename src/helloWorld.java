3fd26ce0496f3d1bd30d0c0abaf46f04f79e75c5
Adminstrator <Adminstrator@example.com>
2021-09-27 12:21:14 +0800

    Revert "dev-1 feature"
    This reverts commit c82cd6bb

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -2,6 +2,5 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is first time");
-        System.out.println("delete change");
     }
 }
