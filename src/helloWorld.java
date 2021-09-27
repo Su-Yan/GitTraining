d78afd3f3fe3af0ea0ca690b36e66303360ab5db
Adminstrator <Adminstrator@example.com>
2021-09-27 12:17:02 +0800

    delete change

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -2,6 +2,6 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is first time");
-        System.out.println("this is dev-1 feature");
+        System.out.println("delete change");
     }
 }
