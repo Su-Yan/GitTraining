f05635d09ebc12c9325733fe506391b85c5c2e3f
Adminstrator <Adminstrator@example.com>
2021-10-13 14:53:30 +0800

    测试

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,14 +1,6 @@
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("this is first time");
-        System.out.println("hello");
-        System.out.println("confilt");
-
-        System.out.println("111");
-        System.out.println("333");
-
-        System.out.println("222");
 
     }
 }
